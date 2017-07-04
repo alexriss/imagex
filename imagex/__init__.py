49a57b7fdf01b14b93726a9c0fd41197aaba8195
Alex Riss <00alexx@riss.at>
2017-07-04 13:12:54 +0200

    Moved __future__ import to the very beginning

--- imagex/__init__.py
+++ imagex/__init__.py
@@ -1,8 +1,9 @@
+from __future__ import absolute_import
+
 __all__ = ["feature", "colormap"]
-__version__ = '0.10.2017-06-12'
+__version__ = '0.10.2017-07-04'
 __author__ = 'Alex Riss'
 
-from __future__ import absolute_import
 from .imagex import *
 from . import colormap as cm
 #from . import feature
